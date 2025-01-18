3e27192a1905dfe0bc984b0b0f37e2e35c8fc088
Md. Nafiur Rahman Protik <protiktee@users.noreply.github.com>
2025-01-18 23:21:14 +0600

    f

--- Ost_inventory_b4/Ost_inventory_b4/Controllers/InventoryController.cs
+++ Ost_inventory_b4/Ost_inventory_b4/Controllers/InventoryController.cs
@@ -54,14 +54,37 @@ namespace Ost_inventory_b4.Controllers
         public ActionResult CustomerAssignment(FormCollection frmColl)
         {
              
+
+
             BaseEquipment baseEquipment= new BaseEquipment();
             int ddlCustomer = Convert.ToInt32(frmColl["ddlCustomer"].ToString());
             int ddlEquipment = Convert.ToInt32(frmColl["ddlEquipment"].ToString());
             int txtEquiCount = Convert.ToInt32(frmColl["txtEquiCount"].ToString());
 
-            baseEquipment.SaveCustomerEquipmentAssignment(frmColl);
-            Session["Message"] = "Save Successfully";
-            return RedirectToAction("Dashboard", "Inventory");
+            List<BaseEquipment> LstEquipment = baseEquipment.LstEquipment();
+
+            var instock = (from a in LstEquipment where a.EquipmentId == ddlEquipment select a.Stock).FirstOrDefault();
+
+            bool status = true;
+            if (instock != null)
+            {
+                if (txtEquiCount > Convert.ToInt32(instock))
+                {
+                    status = false;
+                }
+            }
+            //select top 1 a.Stock from table where EquipmentId=@EquipmentId
+            if (status)
+            {
+                baseEquipment.SaveCustomerEquipmentAssignment(frmColl);
+                Session["Message"] = "Save Successfully";
+                return RedirectToAction("Dashboard", "Inventory");
+            }
+            else
+            {
+                Session["Message"] = "Requested quantity not available";
+                return View();
+            }
         }
     }
 }
