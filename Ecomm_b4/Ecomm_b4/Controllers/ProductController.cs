cf1aca14ab22ad891b9181475976a71e21227c59
Md. Nafiur Rahman Protik <protiktee@users.noreply.github.com>
2025-02-08 23:32:53 +0600

    xz

---
+++ Ecomm_b4/Ecomm_b4/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Ecomm_b4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecomm_b4.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetProducts()
+        {
+            List<BaseProduct> baseProducts = new List<BaseProduct>();
+
+            BaseProduct baseProduct = new BaseProduct();
+            baseProduct.name = "Samsung";
+            baseProduct.tag = "Samsung";
+            baseProduct.Category.CategoryId = 1;
+            baseProduct.Category.Category = "Smartphone";
+            baseProduct.price = 15000;
+
+            BaseReview baseReview = new BaseReview();
+            baseReview.Review = "1st review";
+            baseProduct.baseReviews.Add(baseReview);
+            baseReview = new BaseReview();
+            baseReview.Review = "2st review";
+            baseProduct.baseReviews.Add(baseReview);
+            baseProducts.Add(baseProduct);
+
+            baseProduct = new BaseProduct();
+            baseProduct.name = "Samsung 2";
+            baseProduct.tag = "Samsung 2";
+            baseProduct.Category.CategoryId = 1;
+            baseProduct.Category.Category = "Smartphone";
+            baseProduct.price = 15000;
+            baseProducts.Add(baseProduct);
+            return Json(baseProducts);
+        }
+    }
+}
---
+++ Ecomm_b4/Ecomm_b4/Models/BaseProduct.cs
@@ -0,0 +1,29 @@
+﻿namespace Ecomm_b4.Models
+{
+    public class BaseProduct
+    {
+        //{"name":"Samsung","tag":"Samsung","categoryid":1,"price":15000}
+        public string name { get; set; }
+        public string tag { get; set; }
+        public BaseCategory Category { get; set; }
+        public double price { get; set; }
+        public List<BaseReview> baseReviews { get; set; }
+        public BaseProduct()
+        {
+            baseReviews = new List<BaseReview>();
+            Category=new BaseCategory();
+        }
+    }
+    public class BaseCategory
+    {
+        public int CategoryId { get; set; }
+        public string Category { get; set; }
+
+    }
+    public class BaseReview
+    { 
+        public int ReviewId { get; set; }
+        public string Review { get; set; }
+
+    }
+}
