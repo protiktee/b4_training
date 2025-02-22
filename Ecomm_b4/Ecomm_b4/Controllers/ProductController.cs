8821891a00d42605a28dccf10946b9091385f8e0
Md. Nafiur Rahman Protik <protiktee@users.noreply.github.com>
2025-02-22 23:23:46 +0600

    fgf

--- Ecomm_b4/Ecomm_b4/Controllers/ProductController.cs
+++ Ecomm_b4/Ecomm_b4/Controllers/ProductController.cs
@@ -5,38 +5,47 @@ namespace Ecomm_b4.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        public IActionResult SingleProduct()
         {
+            int id = Convert.ToInt32(HttpContext.Request.Query["id"].ToString());
             return View();
         }
 
-        public JsonResult GetProducts()
-        {
-            List<BaseProduct> baseProducts = new List<BaseProduct>();
+        //[HttpGet]
+        //public JsonResult GetProducts(int value)
+        //{
+        //    List<BaseProduct> baseProducts = new List<BaseProduct>();
 
-            BaseProduct baseProduct = new BaseProduct();
-            baseProduct.name = "Samsung";
-            baseProduct.tag = "Samsung";
-            baseProduct.Category.CategoryId = 1;
-            baseProduct.Category.Category = "Smartphone";
-            baseProduct.price = 15000;
+        //    BaseProduct baseProduct = new BaseProduct();
+        //    baseProduct.name = "Samsung";
+        //    baseProduct.tag = "Samsung";
+        //    baseProduct.Category.CategoryId = 1;
+        //    baseProduct.Category.Category = "Smartphone";
+        //    baseProduct.price = 15000;
 
-            BaseReview baseReview = new BaseReview();
-            baseReview.Review = "1st review";
-            baseProduct.baseReviews.Add(baseReview);
-            baseReview = new BaseReview();
-            baseReview.Review = "2st review";
-            baseProduct.baseReviews.Add(baseReview);
-            baseProducts.Add(baseProduct);
+        //    BaseReview baseReview = new BaseReview();
+        //    baseReview.Review = "1st review";
+        //    baseProduct.baseReviews.Add(baseReview);
+        //    baseReview = new BaseReview();
+        //    baseReview.Review = "2st review";
+        //    baseProduct.baseReviews.Add(baseReview);
+        //    baseProducts.Add(baseProduct);
 
-            baseProduct = new BaseProduct();
-            baseProduct.name = "Samsung 2";
-            baseProduct.tag = "Samsung 2";
-            baseProduct.Category.CategoryId = 1;
-            baseProduct.Category.Category = "Smartphone";
-            baseProduct.price = 15000;
-            baseProducts.Add(baseProduct);
-            return Json(baseProducts);
-        }
+        //    baseProduct = new BaseProduct();
+        //    baseProduct.name = "Samsung 2";
+        //    baseProduct.tag = "Samsung 2";
+        //    baseProduct.Category.CategoryId = 1;
+        //    baseProduct.Category.Category = "Smartphone";
+        //    baseProduct.price = 15000;
+        //    baseProducts.Add(baseProduct);
+        //    return Json(value);
+        //}
+
+        //[HttpPost]
+        //public JsonResult SaveProduct(BaseProduct baseProducts)
+        //{
+        //    int i = 0;
+        //    return Json(new { BaseProduct = baseProducts, PI = i });
+        //}
     }
 }
--- Ecomm_b4/Ecomm_b4/Models/BaseProduct.cs
+++ Ecomm_b4/Ecomm_b4/Models/BaseProduct.cs
@@ -1,29 +1,56 @@
-﻿namespace Ecomm_b4.Models
+﻿using System.Runtime.Serialization;
+
+namespace Ecomm_b4.Models
 {
+    [Serializable] 
     public class BaseProduct
     {
-        //{"name":"Samsung","tag":"Samsung","categoryid":1,"price":15000}
-        public string name { get; set; }
-        public string tag { get; set; }
-        public BaseCategory Category { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
         public double price { get; set; }
-        public List<BaseReview> baseReviews { get; set; }
-        public BaseProduct()
-        {
-            baseReviews = new List<BaseReview>();
-            Category=new BaseCategory();
-        }
+        public double discountPercentage { get; set; }
+        public double rating { get; set; }
+        public int stock { get; set; }
+        public List<string> tags { get; set; }
+        public string brand { get; set; }
+        public string sku { get; set; }
+        public double weight { get; set; }
+        public BaseDimensions Dimensions { get; set; }
+        public string warrantyInformation { get; set; }
+        public string shippingInformation { get; set; }
+        public string availabilityStatus { get; set; }
+        public List<BaseReview> reviews { get; set; }
+        public string returnPolicy { get; set; }
+        public int minimumOrderQuantity { get; set; }
+        public BaseMeta meta { get; set; }
+        public List<string> images { get; set; }
+        public string thumbnail { get; set; }
     }
-    public class BaseCategory
-    {
-        public int CategoryId { get; set; }
-        public string Category { get; set; }
-
-    }
+    [Serializable]
     public class BaseReview
-    { 
-        public int ReviewId { get; set; }
-        public string Review { get; set; }
-
+    {
+        public int ProductID { get; set; }
+        public int rating { get; set; }
+        public string comment { get; set; }
+        public string date { get; set; }
+        public string reviewerName { get; set; }
+        public string reviewerEmail { get; set; }
+    }
+    [Serializable]
+    public class BaseMeta
+    {
+        public string createdAt { get; set; }
+        public string updatedAt { get; set; }
+        public string barcode { get; set; }
+        public string qrCode { get; set; }
+    }
+    [Serializable]
+    public class BaseDimensions
+    {
+        public double width { get; set; }
+        public double height { get; set; }
+        public double depth { get; set; }
     }
 }
